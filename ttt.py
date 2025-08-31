b0d90ea465832ebcbff66a7165a7ac390793fbc6
kunalrohillaz7x <kunal9255r@gmail.com>
2025-08-31 20:00:55 +0530

    almost complete

--- ttt.py
+++ ttt.py
@@ -10,12 +10,63 @@ class TicTacToe:
         player = self.player_name()
         return player[idx]
     
-    def coordinates():
+    def coordinates(self):
         coor = input("Enter your coordinates : ")
         coor = coor.split(",")
         x = int(coor[0])
         y = int(coor[1])
-        return x,y    
+        return x,y  
+    
+    def print_grid(self):
+        l=[[" "," "," "],[" "," "," "],[" "," "," "]]
+        first_person = self.choose_player()
+        for i in range(9):
+            if i%2 == 0:
+                print(f"it's your turn {first_person}")
+                coor = self.coordinates()
+                l[coor[0]][coor[1]] = "X"
+                print(f"""
+                         {l[0][0]} | {l[0][1]} | {l[0][2]}
+                        ---+---+---
+                         {l[1][0]} | {l[1][1]} | {l[1][2]}
+                        ---+---+---
+                         {l[2][0]} | {l[2][1]} | {l[2][2]}
+""")
+                if (l[0][0] == l[0][1] == l[0][2] != " ") or (l[1][0] == l[1][1] == l[1][2] != " ") or (l[2][0] == l[2][1] == l[2][2] != " ") or (l[0][0] == l[1][0] == l[2][0] != " ") or (l[0][1] == l[1][1] == l[2][1] != " ") or (l[0][2] == l[1][2] == l[2][2] != " ") or (l[0][0] == l[1][1] == l[2][2] != " ") or (l[0][2] == l[1][1] == l[2][0] != " "):
+                    print(f"{first_person} WINS! ")
+                    break
+                else:
+                    continue
+            
+            else:
+                print("2nd player's turn")
+                coor = self.coordinates()
+                l[coor[0]][coor[1]] = "O"
+                print(f"""
+                         {l[0][0]} | {l[0][1]} | {l[0][2]}
+                        ---+---+---
+                         {l[1][0]} | {l[1][1]} | {l[1][2]}
+                        ---+---+---
+                         {l[2][0]} | {l[2][1]} | {l[2][2]}
+""")
+                if (l[0][0] == l[0][1] == l[0][2] != " ") or (l[1][0] == l[1][1] == l[1][2] != " ") or (l[2][0] == l[2][1] == l[2][2] != " ") or (l[0][0] == l[1][0] == l[2][0] != " ") or (l[0][1] == l[1][1] == l[2][1] != " ") or (l[0][2] == l[1][2] == l[2][2] != " ") or (l[0][0] == l[1][1] == l[2][2] != " ") or (l[0][2] == l[1][1] == l[2][0] != " "):
+                    print("2nd WINS! ")
+                    break
+                else:
+                    continue
+ttt = TicTacToe()
+ttt.print_grid()
+
+
+
+
+
+
+                
+
+
+
+
     
 
     
