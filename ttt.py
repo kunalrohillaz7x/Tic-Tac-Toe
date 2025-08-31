0a37169f39986261adbc97d4f8a5b1bf36c34c65
kunalrohillaz7x <kunal9255r@gmail.com>
2025-08-31 11:22:52 +0530

    function added to choose player

--- ttt.py
+++ ttt.py
@@ -1,5 +1,15 @@
 class TicTacToe:
-    def player_name():
+    def player_name(self):
         player1 = input("Enter the name of player 1 :")
         player2 = input("Enter the name of player 2 :")
         return player1,player2
+    def choose_player(self):
+        import random
+        idx = random.randint(0,1)
+        player = self.player_name()
+        return player[idx]
+    
+
+
+
+
