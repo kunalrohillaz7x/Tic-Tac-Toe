8f463d38a223755358e728c486e322b2df793496
kunalrohillaz7x <kunal9255r@gmail.com>
2025-08-31 10:58:24 +0530

    name function added

--- ttt.py
+++ ttt.py
@@ -1,1 +1,5 @@
-print("starting my new project")
+class TicTacToe:
+    def player_name():
+        player1 = input("Enter the name of player 1 :")
+        player2 = input("Enter the name of player 2 :")
+        return player1,player2
