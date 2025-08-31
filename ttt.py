71373a273f3b4edd443f656073c1e8cee8d02438
kunalrohillaz7x <kunal9255r@gmail.com>
2025-08-31 18:15:51 +0530

    coordinate input function added

--- ttt.py
+++ ttt.py
@@ -3,11 +3,24 @@ class TicTacToe:
         player1 = input("Enter the name of player 1 :")
         player2 = input("Enter the name of player 2 :")
         return player1,player2
+    
     def choose_player(self):
         import random
         idx = random.randint(0,1)
         player = self.player_name()
         return player[idx]
+    
+    def coordinates():
+        coor = input("Enter your coordinates : ")
+        coor = coor.split(",")
+        x = int(coor[0])
+        y = int(coor[1])
+        return x,y    
+    
+
+    
+    
+
     
 
 
